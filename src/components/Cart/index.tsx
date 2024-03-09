2bfebec042368f807582b3e13311371c8a89af33
Thiago <bussoladev@gmail.com>
2024-03-09 12:19:18 -0300

    feat: cart changes

--- src/components/Cart/index.tsx
+++ src/components/Cart/index.tsx
@@ -1,17 +1,17 @@
 import { useDispatch, useSelector } from 'react-redux';
-import { increaseQuantity, decreaseQuantity, toggleCart} from '../../store/actions/cartActions';
+import { increaseQuantity, decreaseQuantity, toggleCart } from '../../store/actions/cartActions';
 import ProductInfo from '../Product-info';
 import { X } from "@phosphor-icons/react"
 import { CartState } from '../../models/cartState';
 
 const Cart = () => {
     const dispatch = useDispatch();
-    const isCartOpen = useSelector((state: {cart: CartState}) => state.cart.isCartOpen);
-    
-    const cartItems = useSelector((state: {cart: CartState}) => state.cart.items);
+    const isCartOpen = useSelector((state: { cart: CartState }) => state.cart.isCartOpen);
+    const cartItems = useSelector((state: { cart: CartState }) => state.cart.items);
     const totalItems = cartItems.reduce((total, item) => total + item.quantityItem, 0);
+    const calculateTotalPrice = useSelector((state: { cart: CartState }) => state.cart.totalPrice);
 
-    const calculateTotalPrice = useSelector((state: {cart: CartState}) => state.cart.totalPrice);
+    console.log('isCartOpen', isCartOpen)
 
     const handleIncreaseQuantity = (id: number, price: number) => {
         dispatch(increaseQuantity(id, price));
@@ -26,24 +26,36 @@ const Cart = () => {
     };
 
     return (
-        <div className={`absolute top-0 right-0 p-4 bg-white h-screen max-w-80 min-w-96 shadow ${isCartOpen ? 'block' : 'hidden'}`}>
-            <h1 className='text-center font-bold'>Resumo da compra</h1>
-            <div className='font-bold'>Subtotal ({totalItems})</div>
-            <div className='font-bold'>Subtotal ({calculateTotalPrice})</div>
-
-            <div>
-                <div>
-                    <ProductInfo products={cartItems}
-                      handleIncreaseQuantity={(id: number, price: number) => handleIncreaseQuantity(id, price)}
-
-                      handleDecreaseQuantity={(id: number, price: number) => handleDecreaseQuantity(id, price)}
-
-                    showButtons={true} />
-                </div>
-                <button onClick={handleCloseCart} className='absolute top-0 right-0 mt-4 mr-4'><X size={32} /></button>
-                
+        <div className={`absolute top-0 right-0 p-6 bg-white h-screen max-w-80 min-w-96 shadow z-10 ${isCartOpen ? 'block' : 'hidden'}`}>
+        {cartItems.length > 0 ? (
+          <>
+            <h1 className='text-xl font-bold text-violet-900'>Resumo da compra</h1>
+            <div className='flex justify-between p-3 border-b mb-4'>
+              <div className='font-bold text-gray-500'>Subtotal ({`${totalItems} ${totalItems !== 1 ? 'itens' : 'item'}`})</div>
+              <div className='font-bold'>Valor total  <strong>R${calculateTotalPrice}</strong></div>
             </div>
-        </div>
+            <div>
+              <div>
+                <ProductInfo
+                  products={cartItems}
+                  handleIncreaseQuantity={(id: number, price: number) => handleIncreaseQuantity(id, price)}
+                  handleDecreaseQuantity={(id: number, price: number) => handleDecreaseQuantity(id, price)}
+                  isCartPage={true}
+                />
+              </div>
+              <button onClick={handleCloseCart} className='absolute top-0 right-0 mt-4 mr-6'><X size={24} /></button>
+            </div>
+          </>
+        ) : (
+          <>
+            <div className='text-center text-gray-500 mt-8'>Você não tem nenhum produto no carrinho</div>
+            <button onClick={handleCloseCart} className='absolute top-0 right-0 mt-4 mr-6'><X size={24} /></button>
+          </>
+        
+          
+        )}
+      </div>
+      
     );
 };
 
--- src/components/Product-info/index.tsx
+++ src/components/Product-info/index.tsx
@@ -1,4 +1,4 @@
-import {Star, Plus, Minus , Trash} from '@phosphor-icons/react'
+import { Star, Plus, Minus, Trash } from '@phosphor-icons/react'
 import { useDispatch, useSelector } from 'react-redux';
 import { clearCart } from '../../store/actions/cartActions';
 import { CartState, productItem } from '../../models/cartState';
@@ -8,58 +8,72 @@ interface ProductInfoProps {
     handleAddToCart?: (product: productItem) => void;
     handleIncreaseQuantity?: (id: number, price: number) => void;
     handleDecreaseQuantity?: (id: number, price: number) => void;
-    showButtons: boolean;
+    isCartPage: boolean;
 }
 
 
 function renderStars(rate: number) {
     const stars = [];
     for (let i = 0; i < rate; i++) {
-      stars.push(<Star  key={i} size={20} color="yellow" />);
+        stars.push(<Star key={i} size={20} color="yellow" />);
     }
     return stars;
-  }
+}
 
 
-export default function ProductInfo({ products, handleAddToCart, handleIncreaseQuantity, handleDecreaseQuantity, showButtons }: ProductInfoProps) {
+export default function ProductInfo({ products, handleAddToCart, handleIncreaseQuantity, handleDecreaseQuantity, isCartPage }: ProductInfoProps) {
     const dispatch = useDispatch();
     // const cartItems = useSelector((state: CartState) => state.cart.items);
 
-    const isCartOpen = useSelector((state: {cart: CartState}) => state.cart.isCartOpen);
+    const isCartOpen = useSelector((state: { cart: CartState }) => state.cart.isCartOpen);
 
     const handleClearCart = (productIds: number) => {
         dispatch(clearCart(productIds));
-      }
+    }
 
-      console.log('state', isCartOpen)
+    console.log('state', isCartOpen)
 
     return (
         <>
             {products.map((product) => (
-                <div key={product.id} className="border rounded-md  rounded-t-2xl mb-4">
+                <div key={product.id} className={`relative border rounded-md mb-4 ${isCartPage ? 'rounded' : 'rounded-t-2xl'}`}>
                     <figure>
-                        <img className="min-w-full h-40 object-cover rounded-t-2xl" src={product.image} alt={product.title} />
-                        <figcaption className="m-4 space-y-1.5">
-                            <div className="text-lg font-bold">{product.title}</div>
-                            <div className="text-lg font-bold">R${product.price}</div>
-                            <div className='flex'>
-                            {!showButtons &&  renderStars(product.rate)} 
-                            </div>
-                            {!showButtons &&  <button className="bg-orange-500 hover:bg-orange-600 text-white p-2 w-full rounded-lg "
-                            onClick={() => handleAddToCart && handleAddToCart(product)}>Adicionar ao Carrinho</button>}
-                           
-                        </figcaption>
-                        {showButtons && (
-                            <>
-                                <button onClick={() => handleIncreaseQuantity && handleIncreaseQuantity(product.id, product.price)}><Plus size={32} /></button>
-                                <div>{product.quantityItem}</div> 
+                        <div className={`${isCartPage ? 'flex' : 'inherit'}`}>
+                            <img className={` ${isCartPage ? 'min-w-[176px] max-w-[176px] max-h-[134px] object-cover p-4' : 'min-w-full h-40 object-cover rounded-t-2xl'}`}
+                                src={product.image} alt={product.title} />
+                            <figcaption className="m-4 space-y-1.5">
+                                <div className="text-lg font-bold">{product.title}</div>
+                                <div className="text-lg font-bold">R${product.price}</div>
+                                {isCartPage && <div className='bg-emerald-600 p-1 rounded-md'>
+                                    <span className='text-white text-sm text-center block'>Frete Gratis</span>
+                                </div>}
 
-                                <button onClick={() => handleDecreaseQuantity && handleDecreaseQuantity(product.id, product.price)}><Minus size={32} /></button>
-                            </>
-                        )}
+                                <div className='flex'>
+                                    {!isCartPage && renderStars(product.rate)}
+                                </div>
+                                {!isCartPage && <button className="bg-orange-500 hover:bg-orange-600 text-white p-2 w-full rounded-lg "
+                                    onClick={() => handleAddToCart && handleAddToCart(product)}>Adicionar ao Carrinho</button>}
+
+                            </figcaption>
+                        </div>
+
                     </figure>
-                    {showButtons && <button onClick={() => handleClearCart(product.id)}><Trash size={32} /></button>}
-                    
+
+                    {isCartPage && (
+                        <div>
+                            <div className='flex items-center mx-4 mb-2 space-x-3'>
+                                <span className='text-sm font-bold text-gray-500'>Quantidade:</span>
+                                <button className="border rounded-full" onClick={() => handleDecreaseQuantity && handleDecreaseQuantity(product.id, product.price)}><Minus size={20} color='gray' className='px-0.5' /></button>
+                                <div className='border min-w-9 text-center rounded'>
+                                    <span className='font-bold'>{product.quantityItem}</span>
+                                </div>
+                                <button className="border rounded-full" onClick={() => handleIncreaseQuantity && handleIncreaseQuantity(product.id, product.price)}><Plus size={20} color='gray' className='px-0.5' /></button>
+                            </div>
+                        </div>
+                    )}
+                    {isCartPage && <button className='absolute top-0 right-0 mt-4 mr-4' onClick={() => handleClearCart(product.id)}>
+                        <Trash size={22} /></button>}
+
                 </div>
             ))}
         </>
